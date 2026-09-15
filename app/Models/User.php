42dabb23c34b4e314dc582cfe4b9548cfcc69c6c
Zainal29 <theevil1896@gmail.com>
2026-09-15 11:09:18 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
 
-    protected $fillable = ['name', 'email', 'password', 'role', 'nis_nip'];
+    // protected $fillable = ['name', 'email', 'password', 'role', 'nis_nip'];
     protected $fillable = [
         'name',
         'email',
